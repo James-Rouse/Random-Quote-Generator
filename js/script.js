213fbe6942d201e0d2076dd849dff80ead1bc345
James-Rouse <83789433+James-Rouse@users.noreply.github.com>
2022-05-10 22:32:07 -0400

    Quotes array added

--- js/script.js
+++ js/script.js
@@ -10,7 +10,56 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+let quotes = [  
+  {
+    "quote": "The world isn’t perfect. But it’s there for us, doing the best it can… that’s what makes it so damn beautiful.",
+    "source": "Roy Mustang",
+    "citation": "Fullmetal Alchemist: Brotherhood",
+    "year": 2009
+  },
+  {
+    "quote": "I’ll leave tomorrow’s problems to tomorrow’s me.",
+    "source": "Saitama",
+    "citation": "One Punch Man",
+    "year": 2015
+  },
+  {
+    "quote": "Being lonely is more painful then getting hurt.",
+    "source": "Monkey D. Luffy",
+    "citation": "One Piece",
+    "year": 1999
+  },
+  {
+    "quote": "A dropout will beat a genius through hard work.",
+    "source": "Rock Lee",
+    "citation": "Naruto",
+    "year": 2002
+  },
+  {
+    "quote": "Reject common sense to make the impossible possible.",
+    "source": "Simon",
+    "citation": "Gurren Lagann",
+    "year": 2007
+  },
+  {
+    "quote": "A place where someone still thinks about you is a place you can call home.",
+    "source": "Jiraiya",
+    "citation": "Naruto",
+    "year": 2002
+  },
+  {
+    "quote": "People, who can’t throw something important away, can never hope to change anything.",
+    "source": "Armin Arlert",
+    "citation": "Attack on Titan",
+    "year": 2013
+  },
+  {
+    "quote": "Don't believe in yourself. Believe in me! Believe in the Kamina who believes in you!",
+    "source": "Kamina",
+    "citation": "Gurren Lagann",
+    "year": 2007
+  },
+]
 
 
 /***
